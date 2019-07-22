1f56829436239922b56fe74d4bf901c7fdbca824
mac-win <462894999@qq.com>
2019-07-23 07:55:30 +0800

    maintain-client

--- management/config.py
+++ management/config.py
@@ -5,8 +5,19 @@ Create: 2019-07-22
 Update: 2019-07-22
 Author: zizle
 """
+import os
 from PyQt5.QtCore import QSettings
+SERVER_ADDR = "http://172.20.10.2:8008/"
 VERSION = '2019.1'
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 CLIENT_HEADERS = {"User-Agent": "DAssistant-Client/" + VERSION}
 # app设置
 app_dawn = QSettings('dawn/initial.ini', QSettings.IniFormat)
+# 首页中间Frame最小高度
+HOME_PAGE_MIDDLE_WINDOW_MIN_HEIGHT = 200
+# PDF展示框的尺寸
+PDF_READER_DIALOG_WIDTH = 1000
+PDF_READER_DIALOG_HEIGHT = 600
+HOMEPAGE_REPORT_PAGE_COUNT = 5  # 常规报告一页显示条数
+
+HOMEPAGE_NOTICE_PAGE_COUNT = 6  # 交易通知一页显示条数
--- management/windows/maintenance.py
+++ management/windows/maintenance.py
@@ -12,36 +12,50 @@ from PyQt5.QtCore import Qt
 from threads import RequestThread
 import config
 
+
 class MaintenanceWindow(QWidget):
     def __init__(self):
         super(MaintenanceWindow, self).__init__()
         hor_layout = QHBoxLayout()
-        left_tree = QTreeWidget()
-        left_tree.setColumnCount(1)
-        hor_layout.addWidget(left_tree, alignment=Qt.AlignLeft)
+        self.left_list = QListWidget()
+        self.left_list.clicked.connect(self.left_list_clicked)
+        hor_layout.addWidget(self.left_list, alignment=Qt.AlignLeft)
         layout = QVBoxLayout()
         layout.addLayout(hor_layout)
         self.setLayout(layout)
         # 线程请求菜单
-        self.set_tree_menu()
+        self.get_tree_menu()
 
     def get_tree_menu(self):
         """ get menus """
         headers = config.CLIENT_HEADERS
-        self.tree_thread = RequestThread(
-            url='',
+        self.left_list_thread = RequestThread(
+            url=config.SERVER_ADDR + 'basic/maintenance/',
             method='get',
             headers=headers,
             data=json.dumps({"machine_code": config.app_dawn.value("machine")}),
             cookies=config.app_dawn.value('cookies')
         )
-        self.tree_thread.finished.connect(self.tree_thread.deleteLater)
-        self.tree_thread.response_signal.connect(self.set_tree_menu)
+        self.left_list_thread.finished.connect(self.left_list_thread.deleteLater)
+        self.left_list_thread.response_signal.connect(self.set_list_menu)
+        self.left_list_thread.start()
 
+    def left_list_clicked(self, tree_item):
+        """ click action """
+        item = self.left_list.currentItem()
+        print(item, item.id)
 
-    def set_tree_menu(self, content):
+    def set_list_menu(self, content):
         """ set the left tree navigate"""
         print(content)
+        if content['error']:
+            pass
+        for module in content['data']:
+            menu = QListWidgetItem(self.left_list)
+            menu.setText(module['name'])
+            menu.id = module['id']
+            print(module['subs'])
+
 
 
 
