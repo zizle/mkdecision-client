bf49fb3aa069fe80a938c37752f91f4d3793b872
zizle <zizle_lin@163.com>
2019-07-26 08:02:13 +0800

    New Architecture

---
+++ management/windows/base.py
@@ -0,0 +1,268 @@
+# _*_ coding:utf-8 _*_
+"""
+the base window of project
+Update: 2019-07-25
+Author: zizle
+"""
+import sys
+import json
+import requests
+from PyQt5.QtWidgets import *
+from PyQt5.QtCore import Qt
+from PyQt5.QtGui import QEnterEvent, QPainter, QColor, QPen, QIcon
+
+
+import config
+from piece.base import TitleBar, MenuBar
+from frame.base import NoDataWindow
+from windows.home import HomePageScrollWindow
+from frame.home import HomePage
+# 枚举左上右下以及四个定点
+Left, Top, Right, Bottom, LeftTop, RightTop, LeftBottom, RightBottom = range(8)
+
+
+class Base(QWidget):
+    # 四周边距
+    Margins = 3
+
+    def __init__(self, *args, **kwargs):
+        super(Base, self).__init__(*args, **kwargs)
+        self.resize(1280, 768)
+        self._pressed = False  # 按住鼠标标记
+        self.Direction = None  # 方向标记
+        # 背景透明
+        self.setAttribute(Qt.WA_TranslucentBackground, True)
+        # frame less
+        self.setWindowFlags(self.windowFlags() | Qt.FramelessWindowHint)
+        # 鼠标跟踪(否则与鼠标移动有关的都无法实现)
+        self.setMouseTracking(True)
+        # 布局
+        layout = QVBoxLayout()
+        # margins for resize window
+        layout.setSpacing(0)
+        layout.setContentsMargins(self.Margins, self.Margins, self.Margins, self.Margins)
+        # window title
+        title_bar = TitleBar()
+        # signal slot
+        title_bar.windowMinimumed.connect(self.showMinimized)
+        title_bar.windowMaximumed.connect(self.showMaximized)
+        title_bar.windowNormaled.connect(self.showNormal)
+        title_bar.windowClosed.connect(self.close)
+        title_bar.windowMoved.connect(self.move)
+        self.windowTitleChanged.connect(title_bar.setTitle)
+        self.windowIconChanged.connect(title_bar.setIcon)
+        # menu bar
+        self.menu_bar = MenuBar()
+        self.menu_bar.installEventFilter(self)
+        self.menu_bar.menu_btn_clicked.connect(self.menu_clicked)
+        # add tab container
+        self.tab = QTabWidget()
+        self.tab.setAutoFillBackground(True)  # 设置背景,否则由于受到父窗口的影响导致透明
+        self.tab.setTabBarAutoHide(True)
+        palette = self.tab.palette()
+        palette.setColor(palette.Window, QColor(240, 240, 240))
+        self.tab.setPalette(palette)
+        self.tab.installEventFilter(self) # 事件过滤
+        # add widgets to layout
+        layout.addWidget(title_bar)
+        layout.addWidget(self.menu_bar)
+        layout.addWidget(self.tab)
+        self.setLayout(layout)
+        # set icon and title
+        self.setWindowIcon(QIcon(config.BASE_DIR + "/media/logo.png"))
+        self.setWindowTitle("瑞达期货研究院分析决策系统v" + config.VERSION)
+        # get menus in server
+        self.get_menus()
+        self.count = 0
+
+    def menu_clicked(self, menu):
+        print('windows.base.py {} : '.format(str(sys._getframe().f_lineno)), menu.unum, menu.text())
+        text = menu.text()
+        if text == '首页':
+            tab = HomePage()
+        else:
+            tab = NoDataWindow(name=text)
+        self.tab.clear()
+        self.tab.addTab(tab, text)
+
+
+    def eventFilter(self, obj, event):
+        """事件过滤器,用于解决鼠标进入其它控件后还原为标准鼠标样式"""
+        if isinstance(event, QEnterEvent):
+            self.setCursor(Qt.ArrowCursor)
+        return super(Base, self).eventFilter(obj, event)
+
+    def get_menus(self):
+        try:
+            # 请求主菜单数据
+            response = requests.get(
+                url=config.SERVER_ADDR + "basic/module/",
+                headers=config.CLIENT_HEADERS,
+                data=json.dumps({"machine_code": config.app_dawn.value('machine')})
+            )
+            response_content = json.loads(response.content.decode("utf-8"))
+            message = response_content['message']
+            menu_data = response_content["data"]
+        except Exception as e:
+            QMessageBox.information(self, "获取数据错误", "请检查网络设置.\n{}".format(e), QMessageBox.Yes)
+            return
+        if response.status_code != 200:
+            QMessageBox.information(self, "获取数据错误", message, QMessageBox.Yes)
+            return
+        print('windows.base.py {} : '.format(str(sys._getframe().f_lineno)), "获取主菜单:", menu_data)
+        for item in menu_data:
+            if item['subs']:
+                print(item['name'],'subs')
+            else:
+                menu_btn = QPushButton(item['name'])
+                menu_btn.unum = item['id']
+                self.menu_bar.addMenuButton(menu_btn)
+        self.menu_bar.addStretch()
+
+    def mouseMoveEvent(self, event):
+        """鼠标移动事件"""
+        super(Base, self).mouseMoveEvent(event)
+        pos = event.pos()
+        xPos, yPos = pos.x(), pos.y()
+        wm, hm = self.width() - self.Margins, self.height() - self.Margins
+        if self.isMaximized() or self.isFullScreen():
+            self.Direction = None
+            self.setCursor(Qt.ArrowCursor)
+            return
+        if event.buttons() == Qt.LeftButton and self._pressed:
+            self.resize_window(pos)
+            return
+        if xPos <= self.Margins and yPos <= self.Margins:
+            # 左上角
+            self.Direction = LeftTop
+            self.setCursor(Qt.SizeFDiagCursor)
+        elif wm <= xPos <= self.width() and hm <= yPos <= self.height():
+            # 右下角
+            self.Direction = RightBottom
+            self.setCursor(Qt.SizeFDiagCursor)
+        elif wm <= xPos and yPos <= self.Margins:
+            # 右上角
+            self.Direction = RightTop
+            self.setCursor(Qt.SizeBDiagCursor)
+        elif xPos <= self.Margins and hm <= yPos:
+            # 左下角
+            self.Direction = LeftBottom
+            self.setCursor(Qt.SizeBDiagCursor)
+        elif 0 <= xPos <= self.Margins and self.Margins <= yPos <= hm:
+            # 左边
+            self.Direction = Left
+            self.setCursor(Qt.SizeHorCursor)
+        elif wm <= xPos <= self.width() and self.Margins <= yPos <= hm:
+            # 右边
+            self.Direction = Right
+            self.setCursor(Qt.SizeHorCursor)
+        elif self.Margins <= xPos <= wm and 0 <= yPos <= self.Margins:
+            # 上面
+            self.Direction = Top
+            self.setCursor(Qt.SizeVerCursor)
+        elif self.Margins <= xPos <= wm and hm <= yPos <= self.height():
+            # 下面
+            self.Direction = Bottom
+            self.setCursor(Qt.SizeVerCursor)
+
+    def mousePressEvent(self, event):
+        """鼠标点击事件"""
+        super(Base, self).mousePressEvent(event)
+        if event.button() == Qt.LeftButton:
+            self._mpos = event.pos()
+            self._pressed = True
+
+    def mouseReleaseEvent(self, event):
+        """鼠标弹起事件"""
+        super(Base, self).mouseReleaseEvent(event)
+        self._pressed = False
+        self.Direction = None
+
+    def move(self, pos):
+        if self.windowState() == Qt.WindowMaximized or self.windowState() == Qt.WindowFullScreen:
+            # 最大化或者全屏则不允许移动
+            return
+        super(Base, self).move(pos)
+
+    def paintEvent(self, event):
+        """由于是全透明背景窗口,重绘事件中绘制透明度为1的难以发现的边框,用于调整窗口大小"""
+        super(Base, self).paintEvent(event)
+        painter = QPainter(self)
+        painter.setPen(QPen(QColor(255, 255, 255, 1), 2 * self.Margins))
+        painter.drawRect(self.rect())
+            
+    def resize_window(self, pos):
+        """调整窗口大小"""
+        if self.Direction == None:
+            return
+        mpos = pos - self._mpos
+        xPos, yPos = mpos.x(), mpos.y()
+        geometry = self.geometry()
+        x, y, w, h = geometry.x(), geometry.y(), geometry.width(), geometry.height()
+        if self.Direction == LeftTop:  # 左上角
+            if w - xPos > self.minimumWidth():
+                x += xPos
+                w -= xPos
+            if h - yPos > self.minimumHeight():
+                y += yPos
+                h -= yPos
+        elif self.Direction == RightBottom:  # 右下角
+            if w + xPos > self.minimumWidth():
+                w += xPos
+                self._mpos = pos
+            if h + yPos > self.minimumHeight():
+                h += yPos
+                self._mpos = pos
+        elif self.Direction == RightTop:  # 右上角
+            if h - yPos > self.minimumHeight():
+                y += yPos
+                h -= yPos
+            if w + xPos > self.minimumWidth():
+                w += xPos
+                self._mpos.setX(pos.x())
+        elif self.Direction == LeftBottom:  # 左下角
+            if w - xPos > self.minimumWidth():
+                x += xPos
+                w -= xPos
+            if h + yPos > self.minimumHeight():
+                h += yPos
+                self._mpos.setY(pos.y())
+        elif self.Direction == Left:  # 左边
+            if w - xPos > self.minimumWidth():
+                x += xPos
+                w -= xPos
+            else:
+                return
+        elif self.Direction == Right:  # 右边
+            if w + xPos > self.minimumWidth():
+                w += xPos
+                self._mpos = pos
+            else:
+                return
+        elif self.Direction == Top:  # 上面
+            if h - yPos > self.minimumHeight():
+                y += yPos
+                h -= yPos
+            else:
+                return
+        elif self.Direction == Bottom:  # 下面
+            if h + yPos > self.minimumHeight():
+                h += yPos
+                self._mpos = pos
+            else:
+                return
+        self.setGeometry(x, y, w, h)
+
+    def showMaximized(self):
+        """最大化,要去除上下左右边界,如果不去除则边框地方会有空隙"""
+        super(Base, self).showMaximized()
+        print(self.layout())
+        self.layout().setContentsMargins(0, 0, 0, 0)
+
+    def showNormal(self):
+        """还原,要保留上下左右边界,否则没有边框无法调整"""
+        super(Base, self).showNormal()
+        self.layout().setContentsMargins(self.Margins, self.Margins, self.Margins, self.Margins)
+
+
+
